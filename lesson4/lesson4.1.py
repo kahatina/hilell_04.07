9c014618b77da3e65bb4120c1ebb28ce82fc65f6
kahatina <agatinakatya@gmail.com>
2025-08-07 14:07:34 +0300

    2 update 4.1 commit

--- lesson4/lesson4.1.py
+++ lesson4/lesson4.1.py
@@ -1,12 +1,8 @@
-import copy
-
 lst = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
-
-copy_of_lst = copy.deepcopy(lst)
 
 for n in lst:
     if n == 0:
-        copy_of_lst.remove(n)
-        copy_of_lst.append(0)
+        lst.remove(n)
+        lst.append(0)
 
-print(copy_of_lst)
+print(lst)
