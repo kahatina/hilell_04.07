4e6b4e760b3302907eefc2d953fdda0b41688a93
kahatina <agatinakatya@gmail.com>
2025-08-29 12:52:34 +0300

    8.3 (2) commit

--- lesson8/lesson8.3.py
+++ lesson8/lesson8.3.py
@@ -2,3 +2,9 @@ def find_unique_value(some_list: list) -> float:
     for el in some_list:
         if some_list.count(el) == 1:
             return el
+
+
+assert find_unique_value([1, 2, 1, 1]) == 2, 'Test1'
+assert find_unique_value([2, 3, 3, 3, 5, 5]) == 2, 'Test2'
+assert find_unique_value([5, 5, 5, 2, 2, 0.5]) == 0.5, 'Test3'
+print("ОК")
