ead2c32887705610a669f9dfd5c8928c9a00a35a
kahatina <agatinakatya@gmail.com>
2025-08-29 12:55:20 +0300

    9.1 and 9.2 (2) commit

--- lesson9/lesson9.1.py
+++ lesson9/lesson9.1.py
@@ -9,3 +9,7 @@ def popular_words (text: str, words: list) -> dict:
         result[word] = lst_low_text.count(word)
 
     return result
+
+
+assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near']) == { 'i': 4, 'was': 3, 'three': 0, 'near': 0 }, 'Test1'
+print('OK')
--- lesson9/lesson9.2.py
+++ lesson9/lesson9.2.py
@@ -8,3 +8,9 @@ def difference(*args: float) -> float:
         result = round(max_number - min_number, 2)
 
     return result
+
+assert difference(1, 2, 3) == 2, 'Test1'
+assert difference(5, -5) == 10, 'Test2'
+assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4, 'Test3'
+assert difference() == 0, 'Test4'
+print('OK')
